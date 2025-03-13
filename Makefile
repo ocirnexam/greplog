b2a016bfa474ef04a67d8c1838305e15b43123da
Max Müller <kontakt.maxmueller@outlook.com>
2025-03-13 21:00:19 +0100

    Implemented and concatenation of search attributes so it only gets printed if both attributes are found in line

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ CFLAGS=-Wall -g
 
 all:
 	gcc -o logfind $(CFLAGS) src/main.c
+
+clean:
+	rm -f logfind
--- src/main.c
+++ src/main.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <stdint.h>
 
 #define DATA_LENGTH 500
 
 typedef struct {
 	FILE* logfiles;
 	char** search_attributes;
+	char* all_search_attributes_string;
 	int search_attr_count;
 } Search;
 
-void safe_exit(Search* s, char* message)
+void Search_destroy(Search* s)
 {
 	if(s)
 	{
@@ -30,15 +32,21 @@ void safe_exit(Search* s, char* message)
 		}
 		free(s);
 	}
+}
+
+void safe_exit(Search* s, char* message)
+{
+	Search_destroy(s);
 	fprintf(stderr, "EXIT: %s:%d: %s\n", __FILE__, __LINE__, message);
 	exit(1);
 }
 
-Search* initialize_search(int argc, char* argv[])
+Search* Search_initialize(int argc, char* argv[])
 {
 	Search* s = malloc(sizeof(Search));
 	char* home_dir = getenv("HOME");
 	char* filename = "/.logfind";
+	size_t full_string_length = 0;
 	if (!s)
 	{
 		safe_exit(s, "Failed to allocate memory");
@@ -60,17 +68,31 @@ Search* initialize_search(int argc, char* argv[])
 	{
 		s->search_attributes[i - 1] = malloc(DATA_LENGTH);
 		memcpy(s->search_attributes[i - 1], argv[i], DATA_LENGTH);
+		full_string_length += strlen(argv[i]);
+	}
+	s->all_search_attributes_string = malloc(sizeof(char) * full_string_length + argc);
+	if(!s->all_search_attributes_string)
+	{
+		safe_exit(s, "Failed to allocate memory");
+	}
+	for(int i = 1; i < argc; i++)
+	{
+		strncat(s->all_search_attributes_string, argv[i], strlen(argv[i]));
+		if(i < argc - 1)
+		{
+			strncat(s->all_search_attributes_string, " ", 2);
+		}
 	}
 	return s;
 }
 
-void search_logfiles(Search* search)
+void Search_logfiles(Search* search)
 {
 	char filename[DATA_LENGTH];
 	char line[DATA_LENGTH];
 	int line_count = 0;
 	FILE* logfile;
-	int i = 0;
+	int i = 0, search_attributes_found = 0;
 	// loop over all logfiles stored in ~/.logfind
 	while(fgets(filename, DATA_LENGTH, search->logfiles) != NULL)
 	{
@@ -90,12 +112,17 @@ void search_logfiles(Search* search)
 			// remove trailing newline
 			line[strcspn(filename, "\r\n")] = 0;
 			line_count++;
+			search_attributes_found = 0;
 			for(i = 0; i < search->search_attr_count; i++)
 			{
 				if(strstr(line, search->search_attributes[i]) != NULL)
 				{
-					printf("%s: %s:%d\n", search->search_attributes[i], filename, line_count);
+					search_attributes_found += 1;
 				}
+			}
+			if (search_attributes_found == search->search_attr_count)
+			{
+				printf("%s: %s:%d\n", search->all_search_attributes_string, filename, line_count);
 			}
 		}
 		fclose(logfile);
@@ -110,14 +137,9 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "[ERROR] Usage: ./logfind <search_str1> [-o] <search_str2> ...\n");
 		exit(1);
 	}
-	Search* search = initialize_search(argc, argv);
-	printf("Entered %d search attributes\n", search->search_attr_count);
-	for(int i = 0; i < search->search_attr_count; i++)
-	{
-		printf("Search for: %s\n", search->search_attributes[i]);
-	}
-	search_logfiles(search);
+	Search* search = Search_initialize(argc, argv);
+	Search_logfiles(search);
 
-	safe_exit(search, "Programm exit");
+	Search_destroy(search);
 	return 0;
 }
